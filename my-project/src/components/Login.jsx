d073da0fea64467b45304ed74b07b76c6bb4d0bc
Eng-Omar20 <muhammadomar24nov.2004@gmail.com>
2024-09-24 20:34:05 +0500

    change

--- my-project/src/components/Login.jsx
+++ my-project/src/components/Login.jsx
@@ -1,7 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigator = useNavigate();
