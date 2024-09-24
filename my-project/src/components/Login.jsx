c32f22ac495ee35791056f89c30073ecd69b48d1
Muhammad Saqib Ali <abc@gmail.com>
2024-09-24 20:30:42 +0500

    modified crud component ui

--- my-project/src/components/Login.jsx
+++ my-project/src/components/Login.jsx
@@ -67,7 +67,6 @@ const Login = () => {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-teal-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-zinc-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 onClick={() => navigator("/crud")}
                 className="flex w-full justify-center rounded-md bg-teal-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-zinc-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-500"
               >
